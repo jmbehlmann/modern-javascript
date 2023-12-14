1c13bf6e8270a7d7bf02b3b2cdcbd70dc7b0a02d
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2023-12-14 13:41:10 -0600

    added babel

--- index.js
+++ index.js
@@ -18,3 +18,19 @@ var dayOfWeek = moment().format('dddd');
 var weekDayString = "The day of the week is: ";
 
 console.log(weekDayString + dayOfWeek);
+
+//attach a click listener to a play button
+document.querySelector('button')?.addEventListener('click', async () => {
+	await Tone.start()
+	console.log('audio is ready')
+})
+
+//create a synth and connect it to the main output (your speakers)
+const synth = new Tone.Synth().toDestination();
+
+//play a middle 'C' for the duration of an 8th note
+synth.triggerAttackRelease("C4", "8n");
+
+// Interpolate variable bindings
+var name = "Bob";
+console.log(`Hello ${name}, how are you at ${datetime}?`);
