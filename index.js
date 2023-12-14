78ff8e847bcbe4222a60173104bb2303d521b693
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2023-12-14 12:27:11 -0600

    added moment node module

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("javescriiiiiiiiiiiipt")
+/* global moment */
+
+console.log("javescriiiiiiiiiiiipt");
+
+var datetime = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log(datetime);
+
+var hoursAgo = moment().startOf("day").fromNow();
+
+var startedString = "The day started ";
+console.log(startedString + hoursAgo);
+
+var dayOfWeek = moment().format('dddd');
+var weekDayString = "The day of the week is: ";
+
+console.log(weekDayString + dayOfWeek);
